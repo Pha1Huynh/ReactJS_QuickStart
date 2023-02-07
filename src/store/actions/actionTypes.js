3fa8f6c2380a951e20da85f480aeb15c7d9763ab
Phat Huynh <huynhtanphatleo@gmail.com>
2023-02-07 10:05:14 +0700

    update redux

---
+++ src/store/actions/actionTypes.js
@@ -0,0 +1,7 @@
+const actionTypes = Object.freeze({
+  //allcode
+  FETCH_TOY_SUCCESS: 'FETCH_TOY_SUCCESS',
+  FETCH_TOY_FAILED: 'FETCH_TOY_FAILED',
+});
+
+export default actionTypes;
---
+++ src/store/reducers/adminReducer.js
@@ -0,0 +1,12 @@
+import actionTypes from '../actions/actionTypes';
+const initialState = {
+  allToy: [],
+};
+const adminReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default adminReducer;
