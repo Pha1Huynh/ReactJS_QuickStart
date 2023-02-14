d4c37a6861f8541ec4bf5d72cd6336f75cc3ea21
Pha1Huynh <101861571+Pha1Huynh@users.noreply.github.com>
2023-02-14 23:01:56 +0700

    Update axios2.js

--- src/axios2.js
+++ src/axios2.js
@@ -2,13 +2,12 @@ import axios from 'axios';
 import jwt_decode from 'jwt-decode';
 import dayjs from 'dayjs';
 import 'dayjs/locale/vi';
-import { getToken } from './utils/token';
+import { getToken, deleteToken } from './utils/token';
 
 import { store } from '~/redux';
 import * as actions from '~/store/actions';
 
 dayjs.locale('vi');
-//getToken is a function declared in ultis to get accesstoken and rtefresh token
 let authTokens = getToken();
 const instance = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_URL,
@@ -17,6 +16,7 @@ const instance = axios.create({
 
 instance.interceptors.request.use(async (req) => {
   if (!authTokens) {
+    authTokens = '';
     authTokens = getToken();
     req.headers.Authorization = `Bearer ${authTokens ? authTokens.accessToken : ''}`;
   }
@@ -25,13 +25,21 @@ instance.interceptors.request.use(async (req) => {
   let exp = accessToken.exp;
   let now = dayjs();
   const isExpired = dayjs(exp * 1000).diff(now) < 1;
-  if (!isExpired) return req;
-  //dispatch here to refresh token
-  //   store.dispatch(actions.handleRefreshToken({ refreshToken: authTokens.refreshToken }));
 
-  authTokens = getToken();
-  req.headers.Authorization = `Bearer ${authTokens ? authTokens.accessToken : ''}`;
-  return req;
+  if (!isExpired) {
+    authTokens = getToken();
+    req.headers.Authorization = `Bearer ${authTokens ? authTokens.accessToken : ''}`;
+
+    return req;
+  }
+
+  if (isExpired) {
+    let res = await store.dispatch(actions.handleRefreshToken({ refreshToken: authTokens.refreshToken }));
+
+    authTokens = res.tokens;
+    req.headers.Authorization = `Bearer ${authTokens ? authTokens.accessToken : ''}`;
+    return req;
+  }
 });
 
 instance.interceptors.response.use((response) => {
